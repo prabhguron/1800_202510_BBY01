452d5ea5f1954b34ce43924e8c1bcb9152dc12bb
prabhguron <prabhten@gmail.com>
2025-03-03 23:53:31 -0800

    added login and signup page

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,20 @@
-function sayHello() {
-    
+ const getStartedBtn = document.getElementById("signPageBtn");
+const signUpButton = document.getElementById("signUpBtn");
+
+
+function run(){
+    getStartedBtn.addEventListener("click", function(){
+
+        document.location.href = "Signup.html"
+        
+         } )
 }
-//sayHello();
+
+
+
+signUpButton.addEventListener("click", function (){
+
+console.log("hi")
+
+
+});
