35a954293158b1a81fa47c42a0f012b5eb31a6c1
prabhguron <prabhten@gmail.com>
2025-03-18 00:29:08 -0700

    Inder this is how you will get all the users infomation i console logged it for you just loop through and display them

--- scripts/leaderboard.js
+++ scripts/leaderboard.js
@@ -1,19 +1,20 @@
-import { auth, db } from "./firebase-config.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
-import {
-  doc,
-  getDoc,
-  setDoc,
-  onSnapshot,
-} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
+import { db } from "./firebase-config.js";
+import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
 
-import { getDatabase, ref, set } from "firebase/database";
-
-function writeUserData(userId, name, email, imageUrl) {
-  const db = getDatabase();
-  set(ref(db, 'users/' + userId), {
-    username: name,
-    email: email,
-    profile_picture : imageUrl
-  });
+async function logAllUsers() {
+  try {
+    const usersCollection = collection(db, "users");
+    const querySnapshot = await getDocs(usersCollection);
+    
+    console.log("All users in collection:");
+    querySnapshot.forEach((doc) => {
+      // Log each user with their ID and data
+      console.log(doc.id, " => ", doc.data());
+    });
+  } catch (error) {
+    console.error("Error getting users:", error);
+  }
 }
+
+// Call the function
+logAllUsers();
