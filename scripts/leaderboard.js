a9f5e30a7e0f10e677b5192363363175d4a15d65
westdev <124764028+westudioss@users.noreply.github.com>
2025-03-18 12:24:06 -0700

    leaderboard functions pt1

--- scripts/leaderboard.js
+++ scripts/leaderboard.js
@@ -5,18 +5,37 @@ async function logAllUsers() {
   try {
     const usersCollection = collection(db, "users");
     const querySnapshot = await getDocs(usersCollection);
+    let lb = [];
     
     console.log("All users in collection:");
     //here you will add the each user make your own html then use
     //innerhtml to add it to leaderboard.html
     querySnapshot.forEach((doc) => {
+      let user = doc.data();
+      let name = user.username;
+      let points = user.points;
       // Log each user with their ID and data
       console.log( doc.data().username);
+      lb.push({
+
+          n : name,
+          p : points,
+
+      })
 
     });
   } catch (error) {
     console.error("Error getting users:", error);
   }
+
+  lb.sort(({p:a}, {p:b}) => b-a);
+
+  let str = "<table><tr><th>#</th><th>Name</th><th>Points</th></tr>";
+  for (let i=0; i < length(lb); i++) {
+      str += "<tr>";
+      str += "<td>";
+  }
+
 }
 
 // Call the function
