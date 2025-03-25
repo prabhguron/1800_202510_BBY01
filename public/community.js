076f31e58e5646194535ff087a1ae3f6ff3baed5
westdev <124764028+westudioss@users.noreply.github.com>
2025-03-25 12:44:15 -0700

    live posts update test

--- public/community.js
+++ public/community.js
@@ -51,7 +51,7 @@ async function createPost() {
 
     await addDoc(collection(db, "posts"), {
         username : userInfo.username,
-        text : message.replace(/</,""),
+        text : message.replace(/</,"").replace(/>/,""),
         year : date.getFullYear(),
         month : date.getMonth()+1,
         day : date.getDate(),
